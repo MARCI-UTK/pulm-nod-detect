28758d0b4a962113644b38a7cb1353cb8114d113
Dylan B Lewis <dlewis37@marci0.eecs.utk.edu>
2024-10-02 13:16:35 -0400

    decouple gradients from RPN and ROI

--- app/train_roi.py
+++ app/train_roi.py
@@ -7,11 +7,10 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 
-from src.model.rpn_loss import RegLoss, ClsLoss, ValClsLoss, NatureClsLoss
 from src.util.util import *
 
-from rpn import RPN, get_centers, get_anc_boxes
-from roi import ROI, CropProposals
+from rpn import RPN, get_centers, get_anc_boxes, rpn_iteration
+from roi import ROI, CropProposals, roi_iteration
 from src.model.feature_extractor import FeatureExtractor
 
 import matplotlib.pyplot as plt
@@ -86,116 +85,37 @@ def main():
             for idx, data in enumerate(pbar):
                 rpn_optimizer.zero_grad()
                 roi_optimizer.zero_grad()
-                
-                fname, x, y, bb_y = data
 
-                # This is used for logging 
-                fname = list(fname)
+                # Get region proposals 
+                rpn_loss, fm, cls_scores, anc_locs = rpn_iteration(data, fe, rpn, rpn_optimizer)
 
-                # Make labels correct size 
-                y = y.unsqueeze(1)
-
-                x = x.to(f'cuda:{fe.device_ids[0]}')
-                y = y.to(f'cuda:{rpn.device_ids[0]}')
-                bb_y = bb_y.to(f'cuda:{rpn.device_ids[0]}')
-                            
-                rpn_optimizer.zero_grad()
-                roi_optimizer.zero_grad()
-                # FE + RPN OPERATIONS 
-
-                fm = fe(x)
-                pred_anch_locs, pred_cls_scores = rpn(fm)
- 
-                sampled_pred, sampled_target = sample_anchor_boxes(pred_cls_scores, y)
-
-                pos_weight = get_pos_weight_val(sampled_target)
-
-                rpn_cls_loss = F.binary_cross_entropy_with_logits(sampled_pred, sampled_target, pos_weight=pos_weight, reduction='mean')
-                rpn_cls_loss = 0.5 * rpn_cls_loss
-
-                rpn_reg_loss = F.smooth_l1_loss(pred_anch_locs, bb_y, beta=1, reduction='none')
-
-                mask = torch.where(y > 0, 1, 0)
-                mask = mask.permute(0, 2, 1)
-
-                rpn_reg_loss = rpn_reg_loss * mask
-                rpn_reg_loss = rpn_reg_loss.sum() / (rpn_reg_loss != 0).sum()
-
-                rpn_loss = rpn_cls_loss + rpn_reg_loss
-
-                rpn_loss.backward(retain_graph=True)
-
-                rpn_train_loss += rpn_loss.item()
-
-                # BEGIN ROI OPERATIONS 
-
-                top_n = 2500
-
-                start = time.time()
-                corners, mask, indexs = rpn_to_roi(cls_scores=pred_cls_scores, pred_locs=pred_anch_locs, 
-                                                   anc_boxes=anc_box_list, nms_thresh=0.1, top_n=top_n)
-                end = time.time()
-                #print(f'IOU NMS time: {end - start}.')
-
-                start = time.time()
-                proposals = crp(fm, corners)
-                end = time.time()
-                #print(f'Crop proposals time: {end - start}.')
-
-                pred_cls_scores, pred_anch_locs = roi(proposals)
-
-                top_n_y = []
-                top_n_bb_y = []
-                final_mask = []
-                y = y.squeeze()
-                pred_cls_scores = pred_cls_scores.squeeze()
-                for i in range(len(y)): 
-                    y_i = y[i]
-                    b_i = bb_y[i] 
-                    m_i = mask[i]
-
-                    top_n_y.append(y_i[indexs[i]])
-                    top_n_bb_y.append(b_i[indexs[i]])
-                    final_mask.append(m_i[indexs[i]])
-
-                top_n_y = torch.stack(top_n_y)
-                top_n_bb_y = torch.stack(top_n_bb_y)
-                final_mask = torch.stack(final_mask)
-
-                update_cm(top_n_y, pred_cls_scores, train_cm)
-
-                final_mask = final_mask & (top_n_y != -1)
-    
-                pos_weight = get_pos_weight_val(top_n_y)
-
-                roi_cls_loss = F.binary_cross_entropy_with_logits(pred_cls_scores, top_n_y, pos_weight=pos_weight, reduction='none')
-                roi_cls_loss = roi_cls_loss.clone() * final_mask.float()
-                roi_cls_loss = roi_cls_loss.sum() / (roi_cls_loss != 0).sum()
-                roi_cls_loss = 0.5 * roi_cls_loss
-
-                final_mask = final_mask & (top_n_y == 1)
-
-                roi_reg_loss = F.smooth_l1_loss(pred_anch_locs, top_n_bb_y, reduction='none', beta=1)
-                roi_reg_loss = roi_reg_loss.clone() * final_mask.unsqueeze(2).float()
-
-                if (roi_reg_loss != 0).sum() == 0: 
-                    roi_reg_loss = 0
-                else: 
-                    roi_reg_loss = roi_reg_loss.sum() / (roi_reg_loss != 0).sum()
-
-                roi_loss = roi_reg_loss + roi_cls_loss
-
-                roi_loss.backward()
-
-                roi_train_loss += roi_loss.item()
-
-                # Clip gradients and update parameters  
+                # Backprop and update parameters for RPN
+                rpn_loss.backward()
                 torch.nn.utils.clip_grad_norm_(fe.parameters(), 1.0)
                 torch.nn.utils.clip_grad_norm_(rpn.parameters(), 1.0)
-                torch.nn.utils.clip_grad_norm_(roi.parameters(), 1.0)
+                rpn_optimizer.step()
 
-                rpn_optimizer.step() 
+                # Detach tensors needed for ROI operations from computation graph of RPN 
+                _, _, y, bb_y = data
+                y = y.detach().requires_grad_(True)
+                bb_y = bb_y.detach().requires_grad_(True)
+                fm = fm.detach().requires_grad_(True)
+                cls_scores = cls_scores.detach().requires_grad_(True)
+                anc_locs = anc_locs.detach().requires_grad_(True)
+
+                anc_box_list = anc_box_list.to(f'cuda:{roi.device_ids[0]}')
+                
+                # Classify output of RPN using ROI
+                roi_loss = roi_iteration(y, bb_y, fm, anc_box_list, roi, crp, 
+                                         roi_optimizer, cls_scores, anc_locs, 2500)
+                
+                # Backprop and update parameters for ROI
+                roi_loss.backward()
+                torch.nn.utils.clip_grad_norm_(roi.parameters(), 1.0)
                 roi_optimizer.step()
+                
+                rpn_train_loss += rpn_loss.item()
+                roi_train_loss += roi_loss.item()
 
                 pbar.set_postfix(rpn_loss=rpn_loss.item(), roi_loss=roi_loss.item(), lr=rpn_optimizer.param_groups[0]['lr'])
 
@@ -203,98 +123,16 @@ def main():
         rpn.eval()
         roi.eval()
         for idx, data in enumerate(val_loader):
-            fname, x, y, bb_y = data
+            # Get region proposals 
+            rpn_loss, fm, cls_scores, anc_locs = rpn_iteration(data, fe, rpn, rpn_optimizer)
 
-            # This is used for logging 
-            fname = list(fname)
-
-            # Make labels correct size 
-            y = y.unsqueeze(1)
-
-            x = x.to(f'cuda:{fe.device_ids[0]}')
-            y = y.to(f'cuda:{rpn.device_ids[0]}')
-            bb_y = bb_y.to(f'cuda:{rpn.device_ids[0]}')
-                        
-            fm = fe(x)
-            pred_anch_locs, pred_cls_scores = rpn(fm)
-
-            sampled_pred, sampled_target = sample_anchor_boxes(pred_cls_scores, y) 
-
-            pos_weight = get_pos_weight_val(sampled_target)
-
-            rpn_cls_loss = F.binary_cross_entropy_with_logits(sampled_pred, sampled_target, pos_weight=pos_weight, reduction='mean')
-            rpn_cls_loss = 0.5 * rpn_cls_loss
-
-            rpn_reg_loss = F.smooth_l1_loss(pred_anch_locs, bb_y, beta=1, reduction='none')
-
-            mask = torch.where(y > 0, 1, 0)
-            mask = mask.permute(0, 2, 1)
-
-            rpn_reg_loss = rpn_reg_loss * mask
-            rpn_reg_loss = rpn_reg_loss.sum() / (rpn_reg_loss != 0).sum() 
-
-            rpn_loss = rpn_cls_loss + rpn_reg_loss
-
-            rpn_val_loss += rpn_loss.item()
-
-            # BEGIN ROI OPERATIONS 
-
-            top_n = 250
-
-            start = time.time()
-            corners, mask, indexs = rpn_to_roi(cls_scores=pred_cls_scores, pred_locs=pred_anch_locs, 
-                                        anc_boxes=anc_box_list, nms_thresh=0.1, top_n=top_n)
-            end = time.time()
-            #print(f'IOU NMS time: {end - start}.')
-
-            start = time.time()
-            proposals = crp(fm, corners)
-            end = time.time()
-            #print(f'Crop proposals time: {end - start}.')
-
-            pred_cls_scores, pred_anch_locs = roi(proposals)
-
-            top_n_y = []
-            top_n_bb_y = []
-            final_mask = []
-            y = y.squeeze()
-            pred_cls_scores = pred_cls_scores.squeeze()
-            for i in range(len(y)): 
-                y_i = y[i]
-                b_i = bb_y[i] 
-                m_i = mask[i]
-
-                top_n_y.append(y_i[indexs[i]])
-                top_n_bb_y.append(b_i[indexs[i]])
-                final_mask.append(m_i[indexs[i]])
-
-            top_n_y = torch.stack(top_n_y)
-            top_n_bb_y = torch.stack(top_n_bb_y)
-            final_mask = torch.stack(final_mask)
-
-            update_cm(top_n_y, pred_cls_scores, val_cm)
-
-            final_mask &= (top_n_y != -1)
+            anc_box_list = anc_box_list.to(f'cuda:{roi.device_ids[0]}')
             
-            pos_weight = get_pos_weight_val(top_n_y)
-
-            roi_cls_loss = F.binary_cross_entropy_with_logits(pred_cls_scores, top_n_y, pos_weight=pos_weight, reduction='none')
-            roi_cls_loss = roi_cls_loss.clone() * final_mask.float()
-            roi_cls_loss = roi_cls_loss.sum() / (roi_cls_loss != 0).sum()
-            roi_cls_loss = 0.5 * roi_cls_loss
-
-            final_mask = final_mask & (top_n_y == 1)
-
-            roi_reg_loss = F.smooth_l1_loss(pred_anch_locs, top_n_bb_y, reduction='none', beta=1)
-            roi_reg_loss = roi_reg_loss.clone() * final_mask.unsqueeze(2).float()
-
-            if (roi_reg_loss != 0).sum() == 0: 
-                roi_reg_loss = 0
-            else: 
-                roi_reg_loss = roi_reg_loss.sum() / (roi_reg_loss != 0).sum()
-
-            roi_loss = roi_reg_loss + roi_cls_loss
-
+            # Classify output of RPN using ROI
+            roi_loss = roi_iteration(y, bb_y, fm, anc_box_list, roi, crp, 
+                                        roi_optimizer, cls_scores, anc_locs, 2500)
+         
+            rpn_val_loss += rpn_loss.item()
             roi_val_loss += roi_loss.item() 
 
         # Take average of losses using the number of batches
